9038511a68bb8f81efb8ffd73419789ff9676cd1
michaelcrg <michaelchiereghin>
2024-10-03 17:11:40 +0200

    Initial commit

---
+++ MEMORY/Readme.md
@@ -0,0 +1,66 @@
+
+**Progetto finale FRONT-END DEVELOPER 2024**
+Codice: 002PROTD2437857
+
+# Memory Card game
+
+## Descrizione
+Il Memory Card Game è un gioco di abbinamento in cui i giocatori devono trovare coppie di carte identiche tra un insieme di carte coperte. Questo progetto è sviluppato utilizzando HTML, CSS e JavaScript.
+
+## Obiettivi e funzionalità
+- **Interfaccia Utente Intuitiva**: Layout semplice e facile da usare.
+- **Animazioni**: Effetti di transizione per il capovolgimento delle carte.
+- **Timer**: Traccia il tempo impiegato per completare il gioco.
+- **Punteggio**: Mostra il punteggio del giocatore.
+
+## Tecnologie Utilizzate
+- **HTML**: Struttura del gioco e layout delle carte.
+- **Tailwind**: Stile delle carte.
+- **CSS**: Animazioni.
+- **JavaScript**: Logica del gioco, gestione degli eventi, controllo del DOM.
+- **DOM Manipulation**: Aggiornamenti dinamici dell'interfaccia utente.
+- **Event Handling**: Gestione degli eventi come click sulle carte.
+
+## Istruzioni per l'Uso
+1. **Inizia il Gioco**: Clicca su una carta per rivelarla.
+2. **Trova le Coppie**: Cerca di trovare le coppie di carte identiche.
+3. **Completamento del Gioco**: Il gioco termina quando tutte le coppie sono state abbinate.
+4. **Visualizza il Punteggio**: Il punteggio finale viene mostrato al termine del gioco.
+
+## Struttura del Progetto
+
+- `index.html`: Contiene la struttura HTML del gioco.
+- `styles.css`: Contiene gli stili CSS per le animazioni.
+- `script.js`: Contiene la logica del gioco in JavaScript.
+
+## Mappa del codice
+**Versione1.0**
+Creare la struttura base del gioco con una griglia di carte e un’area per il punteggio e il timer.
+Stilizzare la griglia delle carte e l’area del punteggio per un layout pulito e responsivo.
+Inizializzare il gioco con la logica di base per gestire il setup delle carte e il comportamento al click.
+
+**Versione 2.0**
+Scrivere la logica per mescolare le carte e distribuirle casualmente nella griglia.
+Implementare la funzionalità per scoprire le carte e controllare se le coppie sono state abbinate correttamente.
+Aggiungere il conteggio del punteggio e il timer per monitorare il tempo di gioco.
+
+**Versione 3.0**
+Aggiungere un meccanismo per controllare la fine del gioco e visualizzare un messaggio di vittoria.
+Migliorare lo stile delle carte per evidenziare quando sono scoperte o abbinate.
+Implementare un pulsante per riavviare il gioco e ricaricare la griglia delle carte.
+
+## Roadmap
+- Modalità multigiocatore.
+- Livelli di difficoltà.
+- Funzioni di accessibilità (Modalità per Daltonici, Supporto per Lettori di Schermo).
+- Miglioramenti all'Interfaccia Utente.
+
+## Componenti del gruppo
+- Michael Chiereghin - [Profilo Github](https://github.com/Michaelcrg)
+- Giulio Sciaccianoce - [Profilo Github](https://github.com/GiulioScaccianoce)
+- Nadeisda Giulia Grandi - [Profilo Github](https://github.com/Nadeisda)
+- Francesco Azzurri - [Profilo Github](https://github.com/FrancescoAzzurri)
+- Franck Emmanuel Neghueko Djoumessi - [Profilo Github](https://github.com/FranckEmmanuel89)
+
+## Licenza
+Questo progetto ha licenza privata.
---
+++ MEMORY/finale.js
@@ -0,0 +1,166 @@
+// Selezioniamo i puntatorilet elementiMazzo = document.querySelectorAll('.card');
+const elementiMazzo = document.querySelectorAll(".card");
+const container = document.querySelector(".grid");
+const rigioca = document.getElementById("rigioca");
+const punteggio = document.getElementById("punteggio");
+const timer = document.getElementById("timer");
+const testoCentrale = document.getElementById("testoCentrale");
+
+// Convertiamo le carte da NodeList ad Array
+let mazzo = Array.from(elementiMazzo);
+
+//Prompt di richiesta del nome e successiva stampa in pagina
+let richiestaNome = prompt("Sei pronto? Inserisci il tuo nome?");
+let titolo = document.getElementById("titolo");
+titolo.textContent = `Trova la coppia, ${richiestaNome}!`;
+
+// Variabili che utilizziamo per salvare e successivamente confrontare le carte
+let primaCarta = null;
+let secondaCarta = null;
+
+// Inizializziamo il punteggio a 0
+let score = 0;
+
+// Creiamo le variabili da sfruttare col timer
+let timerInterval;
+let tempo = 0;
+
+// Funzione per mischiare il mazzo
+function mischiaCarte(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let numeroRandom = Math.floor(Math.random() * (i + 1));
+    [array[i], array[numeroRandom]] = [array[numeroRandom], array[i]]; // questa riga serve per mischiare gli indici
+    //invertendo ogni indice con un indice random compreso nella lunghhezza dell'array
+  }
+  return array;
+}
+
+// Funzione che aggiorna il punteggio
+function aggiornaPunteggio() {
+  score++;
+  punteggio.innerText = `Punteggio: ${score}`;
+}
+
+// Funzione che fa partire il timer e lo aggiorna ad ogni secondo
+function avviaTimer() {
+  timerInterval = setInterval(() => {
+    tempo++;
+    let minuti = Math.floor(tempo / 60);
+    let secondi = tempo % 60;
+    timer.innerText = `${minuti < 10 ? "0" + minuti : minuti}:${
+      secondi < 10 ? "0" + secondi : secondi
+    }`;
+  }, 1000);
+}
+//logica per fermare il timer
+function fermaTimer() {
+  clearInterval(timerInterval);
+}
+
+// Funzione di confronto delle coppie
+function controlloCarte() {
+  if (primaCarta.textContent === secondaCarta.textContent) {
+    //se le carte sono uguali si aggiorna il punteggio e
+    aggiornaPunteggio(); //vengono nascoste, si resettano i valori delle variabili
+    setTimeout(() => {
+      //e in caso non vi siano più carte termina il gioco
+      primaCarta.style.visibility = "hidden";
+      secondaCarta.style.visibility = "hidden";
+      resetCarte();
+      verificaFineGioco();
+    }, 500);
+  } else {
+    //se le carte non sono uguali le riporta alla posizione orginale
+    setTimeout(() => {
+      reverseFlip(primaCarta);
+      reverseFlip(secondaCarta);
+      resetCarte();
+    }, 500);
+  }
+}
+//resetta i valori delle variabili di confronto delle carte
+function resetCarte() {
+  primaCarta = null;
+  secondaCarta = null;
+}
+//se tutte le carte sono state rimosse dal terreno e in tal caso cambia la visualizzazione della griglia
+function verificaFineGioco() {
+  if (mazzo.every((carta) => carta.style.visibility === "hidden")) {
+    fermaTimer();
+    modificaCentro();
+  }
+}
+
+// Funzione per iniziare a giocare che faremo partire al caricamento della finestra e poi col rigioca
+function iniziaAGiocare() {
+  resetCarte();
+
+  let mazzoMischiato = mischiaCarte(mazzo);
+  container.innerHTML = ""; // Svuota il container delle carte
+  mazzoMischiato.forEach((carta) => {
+    container.append(carta); //appende le carte una volta mischiate
+    carta.classList.remove("hidden"); //Rende le carte visibili
+
+    carta.textContent = ""; // Nasconde il valore inizialmente
+    carta.addEventListener("click", () => {
+      carta.classList.remove("reverseflip"); //rimozione della classe reverseflip
+      if (!primaCarta) {
+        //
+        flipCard(carta);
+        primaCarta = carta; //qui se non c'è alcuna carta selezionata viene girata ne viene salvato il valore in una variabile di confronto
+      } else if (!secondaCarta && carta !== primaCarta) {
+        secondaCarta = carta; //quando si pesca la seconda viene girata ed entra nella funzione di controllo
+        flipCard(carta);
+
+        controlloCarte();
+      }
+    });
+  });
+}
+// funzione che gira le carte mostrandone il valore e
+function flipCard(carta) {
+  carta.textContent = carta.value;
+  carta.classList.add("flip");
+  carta.classList.remove("bgCard");
+}
+//riporta le carte alla posizione originale
+function reverseFlip(carta) {
+  carta.textContent = "";
+  carta.classList.add("reverseflip");
+  carta.classList.remove("flip");
+  carta.classList.add("bgCard");
+}
+
+// Inizializza il gioco quando la pagina è caricata
+window.onload = () => {
+  iniziaAGiocare();
+  avviaTimer();
+};
+//rende le carte di nuovo visibili
+function rendiTutteLeCarteVisibili() {
+  mazzo.forEach((carta) => {
+    carta.style.visibility = "visible";
+    carta.classList.add("bgCard");
+  });
+}
+//Cambia il contenuto della griglia inserendo il messaggio di vittoria
+function modificaCentro() {
+  titolo.innerText = "";
+  container.classList.remove("grid", "grid-cols-4");
+  container.classList.add("flex", "items-center", "justify-center");
+  container.innerHTML = `Complimenti ${richiestaNome}, hai terminato la partita in ${tempo} secondi.`;
+}
+
+// Aggiungi un listener per il pulsante di reset
+rigioca.addEventListener("click", () => {
+  container.classList.remove("flex", "items-center", "justify-center");
+  container.classList.add("grid", "grid-cols-4");
+  rendiTutteLeCarteVisibili();
+  mischiaCarte(mazzo);
+  score = 0;
+  aggiornaPunteggio();
+  tempo = 0;
+  fermaTimer();
+  iniziaAGiocare();
+  avviaTimer();
+});
